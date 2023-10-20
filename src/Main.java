6442cbe52b3913516b76e5481022f835f9c08a74
attiaimed <imed.attia@esprit.tn>
2023-10-20 16:44:46 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Etudiant e1 = new Etudiant(1,"Attia","Imed");
+        Etudiant e2 = new Etudiant(2,"wassef","chargui");
+        Etudiant e3 = new Etudiant(3,"Khalil" , "Bhym");
+
+        System.out.println(e1.equals(e2));
+        System.out.println(e3);
+        System.out.println(e2.equals(e2));
+
+
+
+
+
+
+
+
+    }
+}
